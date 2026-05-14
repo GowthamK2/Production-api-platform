78aabe0b0ca0263858096a49c7d3db295bbcad84
Gowtham <gowthamkrissh24@gmail.com>
2026-05-14 10:51:56 +0530

    Added product routes and Validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name : str
+    age : int 
+    skills : list[str]
+
+class Product(BaseModel):
+    name: str
+    price: float
+    stock: int 
 
 @app.get("/") 
 def home():
@@ -16,6 +27,7 @@ def about():
 def contact():
     return {
         "name": "Gowtham",
+
         "role": "AI Backend Developer"
     }
 
@@ -24,3 +36,30 @@ def skills():
     return {
         "skills": ["FastAPI", "Python"]
     }
+
+@app.get("/projects")
+def projects():
+    return  {
+        "projects": ["Production API Platform", "AI Chatbot"]
+    }
+
+@app.get("/products")
+def products():
+    return {
+        "products": ["Laptop", "Keyboard"]
+    }
+
+@app.post("/users")
+def create_user(user: User):
+    return {
+        "message": "User Created",
+        "data": user
+    }
+
+@app.post("/products")
+def create_product(product: Product):
+    return {
+        "message": "Product Created",
+        "data": product 
+    }
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -23,3 +23,42 @@ def test_skills():
     assert response.json() == {
         "skills": ["FastAPI", "Python"]
     }
+
+def test_projects():
+    response = client.get("/projects")
+    assert response.status_code == 200
+
+def test_get_product():
+    response = client.get("/products")
+    assert response.status_code == 200
+    assert response.json() == {
+        "products": ["Laptop", "Keyboard"]
+    }
+
+
+def test_create_user():
+    response = client.post("/users", 
+                           json = {
+                               "name": "Gowtham",
+                               "age": 25,
+                               "skills": ["FastAPI", "Python"]
+                           }
+                        )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["message"] == "User Created"
+    assert data["data"]["name"] == "Gowtham"
+
+def test_create_product():
+    response = client.post(
+        "/products",
+        json= {
+                "name": "Laptop",
+                "price": 299999.00,
+                "stock": 54
+            }
+        )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["message"] == "Product Created"
+    assert data["data"]["name"] == "Laptop" 
